f6ff2b4d7483b814fde8ae2e054435e91d7d63f0
Toni Matkovic <tmatkovic@optimizerx.com>
2020-10-13 16:25:48 +0200

    Added lodash, css styling & answer shuffling

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -10,18 +10,21 @@
         <b-list-group-item
             v-for="(answer, index) in answers" :key="index"
             @click="selectAnswer(index)"
+            :class="[selectedIndex === index ? 'selected' : '']"
         >
           {{ answer }}
         </b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#" @click="next">Next</b-button>
+      <b-button variant="primary">Submit</b-button>
+      <b-button variant="success" @click="next">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   props: {
     currentQuestion: Object,
@@ -29,19 +32,32 @@ export default {
   },
   data() {
     return {
-      selectedIndex: null
+      selectedIndex: null,
+      shuffledAnswers: []
     }
   },
   computed: {
     answers() {
-      let answers = [...this.currentQuestion.incorrect_answers];
-      answers.push(this.currentQuestion.correct_answer);
-      return answers;
+      return [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+    }
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.shuffleAnswers();
+      }
     }
   },
   methods: {
     selectAnswer(index) {
-      this.selectedIndex =index;
+      this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+      this.shuffledAnswers = _.shuffle(answers);
+      return answers;
     }
   }
 }
@@ -51,12 +67,25 @@ export default {
 .list-group {
   margin-bottom: 15px;
 }
+
 .list-group-item:hover {
-background: #EEE;
-  cursor:pointer;
+  background: #EEE;
+  cursor: pointer;
 }
 
 .btn {
   margin: 0 5px;
+}
+
+.selected {
+  background: lightblue;
+}
+
+.correct {
+  background: lightgreen;
+}
+
+.incorrect {
+  background: red;
 }
 </style>
