459217f6264b06217d639ee0fa3faa09ce5eb536
Toni Matkovic <tmatkovic@optimizerx.com>
2020-10-13 14:59:47 +0200

    playing around

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>
+        {{ currentQuestion.question }}
+      </template>
+      <hr class="my-4">
+      <ul>
+        <li v-for="answer in currentQuestion.incorrect_answers" v-bind:key="answer">
+          {{ answer }}
+        </li>
+        <li>
+          {{ currentQuestion.correct_answer}}
+        </li>
+      </ul>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+  }
+}
+</script>
